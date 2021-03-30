5d763ae7a705b6b0d9225ac578c9d03b9911943a
eyeshield2110 <ndinh@hotmail.fr>
2021-03-30 11:12:22 -0400

    testing links

--- app.php
+++ app.php
@@ -27,3 +27,18 @@ echo 'Protocol version: '.$mysqli->protocol_version;
 
 $mysqli->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+<br>
+<a href="./anothertest.php">Link to "anothertest.php"</a>
+
+</body>
+</html>
