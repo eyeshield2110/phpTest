e87419e91fbd9da7ac59009449bad6291f334d8a
Noah Dinh <noah.dinh@outlook.com>
2021-04-11 12:45:20 -0400

    Credentials : try again

--- connect_partials.php
+++ connect_partials.php
@@ -4,12 +4,24 @@ $username = "uec353_4";
 $password = "c0NcR6iA";
 $dbname = "test";
 
+
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-    exit();
+    // exit();
+    $db_host = 'uec353.encs.concordia.ca';
+    $db_user = 'uec353_4';
+    $db_password = 'c0NcR6iA';
+    $db_db = 'uec353_4';
+    $db_port = 3306;
+    $mysqli = new mysqli($db_host, $db_user, $db_password, $db_db);
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to AITS";
+    } else {
+        echo "<script>console.log('connected to AITS mysql')</script>";
+    }
 } else {
     echo "<script>console.log('connected to mysql')</script>";
 }
