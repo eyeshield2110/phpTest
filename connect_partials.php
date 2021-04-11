f33606f6760e9f710e949dde602c487afcf4b50d
Noah Dinh <noah.dinh@outlook.com>
2021-04-11 12:42:52 -0400

    Rewrote original credential (alt didn't work)

--- connect_partials.php
+++ connect_partials.php
@@ -8,20 +8,8 @@ $mysqli = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($mysqli->connect_errno) {
-    echo "Failed to connect to local MySQL: " . $mysqli->connect_error;
-    // exit();
-    $db_host = 'uec353.encs.concordia.ca';
-    $db_user = 'uec353_4';
-    $db_password = 'c0NcR6iA';
-    $db_db = 'uec353_4';
-    $db_port = 3306;
-    $mysqli = new mysqli($servername, $username, $password, $dbname);
-
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to AITS server mysql";
-    } else {
-        echo "connected to AITS server mysql";
-    }
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
 } else {
     echo "<script>console.log('connected to mysql')</script>";
 }
